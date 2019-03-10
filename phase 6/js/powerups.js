bb78e352ca925642ca62c2b9fd5fa5b8bf6344ec
Dušan Spasić <dusan.spasic1978@gmail.com>
2019-03-10 17:55:35 +0100

    fixed

---
+++ phase 6/js/powerups.js
@@ -0,0 +1,45 @@
+const LASER = 1;
+var laserPostoji = 0;
+var nadogradnja = [];
+
+function kreiranjeNadogradnje(tipNadogradnje, left, top) {
+  if (tipNadogradnje == LASER) {
+    var nagrada = document.createElement("img");
+
+    nagrada.style.position = "absolute";
+    nagrada.style.width = "40px";
+    nagrada.style.height = "40px";
+    nagrada.style.left = left;
+    nagrada.style.top = top;
+    nagrada.src = "img/laser_nagrada.png";
+    nagrada.setAttribute("tip", LASER);
+    nadogradnja.push(nagrada);
+    document.getElementById("poljeZaIgru").appendChild(nagrada);
+  }
+}
+
+function proveraNagrada() {
+  for (var i = 0; i < nadogradnja.length; i++) {
+    pomeranjeNadogradnje(nadogradnja[i]);
+  }
+}
+
+function pomeranjeNadogradnje(nagrada) {
+  var y = nagrada.offsetTop; // vertikalna pozicija
+  if (y > dimenzijePoljaZaIgru.height - 20) {
+    return nagrada.parentNode.removeChild(nagrada);
+  }
+
+  if (detekcijaUdara(nagrada, palica)) {
+    if (nagrada.getAttribute("tip") == LASER) {
+      laserPostoji++;
+      setTimeout(function () {
+      laserPostoji--;
+      }, 13 * 1000);
+      return nagrada.parentNode.removeChild(nagrada);
+    }
+
+  }
+
+  nagrada.style.top = y + 2+ "px";//
+}
---
+++ phase 6/js/records.js
@@ -0,0 +1,13 @@
+var rangLista = localStorage.getItem("rekordi");
+
+var rekordi = [];
+if (rangLista) {
+  rekordi = JSON.parse(rangLista);
+}
+//console.log(rekordi);
+var tabela = document.getElementById("rekordi");
+for (var i = 0; i < rekordi.length; i++) {
+  var redovi = document.createElement("tr");
+  redovi.innerHTML = `<td> ${rekordi[i].ime}</td><td> ${rekordi[i].poeni}</td><td>${rekordi[i].vreme} sek.</td>`;
+  tabela.appendChild(redovi);
+}
